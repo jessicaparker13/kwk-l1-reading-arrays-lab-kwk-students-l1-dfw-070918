a8d5580c862fa1fe6b20c3dac5df55c032e86e51
jessicaparker13 <parkerjessica2001@gmail.com>
2018-07-10 20:31:23 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -32,7 +32,8 @@ def last_student_by_method
   STUDENT_NAMES.last 
 end
 
+S[0, 1, 2]
+end
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  STUDENT_NAMES[0, 1, 2]
-end
+  STUDENT_NAME
